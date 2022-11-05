0dc51ec93aabfd5225e702c5d6b8a940b95a6ea3
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-04 20:05:57 -0400

    Finished AverageIntArrays

--- src/main/java/AverageIntArrays.java
+++ src/main/java/AverageIntArrays.java
@@ -3,15 +3,26 @@ public class AverageIntArrays {
 // Complete these methods
 
     public int count(int[] aa) {
-        return 0;
+        return aa.length;
     }
 
     public int sum(int[] aa) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i <= aa.length-1; i++){
+            sum = sum + aa[i];
+        }
+        return sum;
     }
 
     public int average(int[] aa) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i <= aa.length-1; i++) {
+            sum = sum + aa[i];
+        }
+        if ( aa.length == 0){
+            return 0;
+        }
+        return sum/aa.length;
     }
 
 }
