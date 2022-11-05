a7cde425c9d4161eab67e93258a0077f960816aa
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-05 18:02:53 -0400

    Finished LoadAndStoreTest.java - All Tests Pass

--- src/test/java/LoadAndStoreTest.java
+++ src/test/java/LoadAndStoreTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadAndStoreTest {
@@ -18,31 +21,70 @@ class LoadAndStoreTest {
 
     @Test
     void loadIntArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+
+        int[] expected = {4,5,6,11,13,8,9,7,14,18};
+
+        int[] actual = loadAndStore.loadIntArrayFromFile("testIntegerData1.txt");
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void loadIntegerArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+        Integer[] array = {4,5,6,11,13,8,9,7,14,18};
+
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(array));
+
+        ArrayList<Integer> actual = loadAndStore.loadIntegerArrayListFromFile("testIntegerData1.txt");
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void loaddoubleArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+
+        double[] expected = {4.4,5.5,6.34,11.0001,13.004,8.7,9.97060,7.4532,14.5,18.00023132123};
+
+        double[] actual = loadAndStore.loadDoubleArrayFromFile("testDoubleData2.txt");
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void loadDoubleArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+        Double[] array = {4.4,5.5,6.34,11.0001,13.004,8.7,9.97060,7.4532,14.5,18.00023132123};
+
+        ArrayList<Double> expected = new ArrayList<>(Arrays.asList(array));
+
+        ArrayList<Double> actual = loadAndStore.loadDoubleArrayListFromFile("testDoubleData2.txt");
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+
+        String[] expected = {"4.4", "5.5", "Foo", "11.0001", "Bar", "Lost in Space", "Lido Shuffle", "How much data was destroyed?", "I think therefore I compute", "0", "NaN", "ZipCode Rocks!"};
+
+        String[] actual = loadAndStore.loadStringArrayFromFile("testStringData3.txt");
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore loadAndStore = new LoadAndStore();
+        String[] array = {"4.4", "5.5", "Foo", "11.0001", "Bar", "Lost in Space", "Lido Shuffle", "How much data was destroyed?", "I think therefore I compute", "0", "NaN", "ZipCode Rocks!"};
+
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList(array));
+
+        ArrayList<String> actual = loadAndStore.loadStringArrayListFromFile("testStringData3.txt");
+
+        assertEquals(expected, actual);
     }
 }
